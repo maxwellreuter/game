86e5c2a93f892b371f1a785763211c7f51ce7cac
Max Reuter <maxwellreuter@gmail.com>
2019-07-09 20:52:57 -0400

    Update ValidateSetup and README

--- GameEngine/src/engineTester/ValidateSetup.java
+++ GameEngine/src/engineTester/ValidateSetup.java
@@ -1,26 +1,52 @@
-// Opens an empty window to validate that the project is setup correctly
+// Manages the main game loop
 
 package engineTester;
 
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class ValidateSetup {
 	
 	public static void main(String[] args) {
 		
 		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
+		
+		// OpenGL expects vertices to be defined counter-clockwise by default
+		float[] vertices = {
+				-0.5f, 0.5f, 0, 	// V0
+				-0.5f, -0.5f, 0,	// V1
+				0.5f, -0.5f, 0, 	// V2
+				0.5f, 0.5f, 0 		// V3
+		};
+		
+		int[] indices = {
+				0, 1, 3,	// top-left triangle (V0, V1, V3)
+				3, 2, 1		// bottom-right triangle (V3, V1, V2)
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, indices);
 		
 		// persist display until user exit
 		while(!Display.isCloseRequested()) {
-			
 			// game logic
-			//render
+			renderer.prepare();
+			shader.start();
+			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 			
 		}
 		
+		shader.cleanUp();
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 		
 	}
--- README.md
+++ README.md
@@ -4,13 +4,15 @@ A 3D Java game engine!
 
 ## Quick-start guide
 
+Work in progress!
+
 1. Clone this GitHub repository: `git clone https://github.com/maxwellreuter/game.git`
 
 2. Get the Eclipse IDE to manage Java code (try `www.eclipse.org/downloads/packages`).
 
 3. Open Eclipse and do `File -> Open Projects from File System...`, then navigate to the `GameEngine` folder, select it, and confirm.
 
-4. Configure your Eclipse project starting in the `game` directory as done here: `https://youtu.be/Jdkq-aSFEA0?t=86`. It may be a bit tricky to get Eclipse to recognize the library properly.
+4. Configure your Eclipse project starting in the `game` directory as done here: `https://youtu.be/Jdkq-aSFEA0?t=86`.
 
 5. Right-click the project (`GameEngine`), then do `Build Path -> Configure Build Path`, then drag the three `.jar` files from under `Modulepath` to under `Classpath`. Then, you might need to click on the JRE System Library in the same window, then click `Edit...` and select your JRE again. If one doesn't work, try another.
 
