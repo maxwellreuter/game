16c899b96555db553975a14aa6b201e9c90fc53c
Max Reuter <maxwellreuter@gmail.com>
2020-04-27 23:33:03 -0400

    Add 3D model loading funcionality

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -11,6 +11,7 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -23,97 +24,19 @@ public class MainGameLoop {
 		Loader loader = new Loader();
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
+	
 		
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,0.5f,-0.5f,		
-				
-				-0.5f,0.5f,0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-				
-		};
+		RawModel model = OBJLoader.loadObjModel("stall", loader);
 		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
+		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("stallTexture")));
 		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-		
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		
-		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("blink-182")));
-		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
 		
 		Camera camera = new Camera();
 		
 		// persist display until user exit
 		while(!Display.isCloseRequested()) {
-			entity.increaseRotation(1, 1, 0);
+			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
--- GameEngine/src/engineTester/ValidateSetup.java
+++ GameEngine/src/engineTester/ValidateSetup.java
@@ -4,60 +4,21 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
-import models.RawModel;
-import models.TexturedModel;
 import renderEngine.DisplayManager;
-import renderEngine.Loader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
-import textures.ModelTexture;
+
 
 public class ValidateSetup {
 	
 	public static void main(String[] args) {
 		
 		DisplayManager.createDisplay();
-		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
-		StaticShader shader = new StaticShader();
-		
-		// OpenGL expects vertices to be defined counter-clockwise by default
-		float[] vertices = {
-				-0.5f, 0.5f, 0, 	// V0
-				-0.5f, -0.5f, 0,	// V1
-				0.5f, -0.5f, 0, 	// V2
-				0.5f, 0.5f, 0 		// V3
-		};
-		
-		int[] indices = {
-				0, 1, 3,	// top-left triangle (V0, V1, V3)
-				3, 1, 2		// bottom-right triangle (V3, V1, V2)
-		};
-		
-		float[] textureCoords = {
-				0,0,	//V0
-				0,1,	//V1
-				1,1,	//V2
-				1,0,	//V3
-		};
-		
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("blink-182"));
-		TexturedModel texturedModel = new TexturedModel(model,texture);
 		
 		// persist display until user exit
 		while(!Display.isCloseRequested()) {
-			// game logic
-			renderer.prepare();
-			shader.start();
-			renderer.render(texturedModel);
-			shader.stop();
 			DisplayManager.updateDisplay();
 			
 		}
 		
-		shader.cleanUp();
-		loader.cleanUp();
 		DisplayManager.closeDisplay();
 		
 	}
