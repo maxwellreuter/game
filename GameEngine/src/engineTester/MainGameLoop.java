2d91b1702762a3b1ef46c72406add42938f23479
Max Reuter <maxwellreuter@gmail.com>
2020-04-28 09:55:39 -0400

    Add lighting functionality

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -7,6 +7,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -30,7 +31,8 @@ public class MainGameLoop {
 		
 		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("stallTexture")));
 		
-		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-25),0,0,0,1);
+		Light light = new Light(new Vector3f(0,0,-20),new Vector3f(1,1,1));
 		
 		Camera camera = new Camera();
 		
@@ -40,6 +42,7 @@ public class MainGameLoop {
 			camera.move();
 			renderer.prepare();
 			shader.start();
+			shader.loadLight(light);
 			shader.loadViewMatrix(camera);
 			renderer.render(entity,shader);
 			shader.stop();
