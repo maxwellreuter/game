54b32e380ea0e35db027defdc5b48cd5d5165216
Max Reuter <maxwellreuter@gmail.com>
2020-05-03 17:03:45 -0400

    Make camera independent of player rotation

--- GameEngine/src/entities/Camera.java
+++ GameEngine/src/entities/Camera.java
@@ -12,7 +12,7 @@ public class Camera {
 	private final float PITCH_MAX = 75;
 	
 	private float distanceFromPlayer = 40;
-	private float angleAroundPlayer = 0;
+	private float angleAroundPlayer = 180;
 	
 	private Vector3f position = new Vector3f(1, 5, 1);
 	private float pitch = 20;
@@ -51,13 +51,16 @@ public class Camera {
 	}
 	
 	private void calculateCameraPosition(float horizontalDistance, float verticalDistance) {
-		float theta = player.getRotY() + angleAroundPlayer;
+		// substitute in commented-out RotY lines to make the camera rotate when the player does
+		//float theta = player.getRotY() + angleAroundPlayer;
+		float theta = angleAroundPlayer;
 		float offsetX = (float) (horizontalDistance * Math.sin(Math.toRadians(theta)));
 		float offsetZ = (float) (horizontalDistance * Math.cos(Math.toRadians(theta)));
 		position.x = player.getPosition().x - offsetX;
 		position.z = player.getPosition().z - offsetZ;
 		position.y = player.getPosition().y + verticalDistance + Y_OFFSET;
-		this.yaw = 180 - (player.getRotY() + angleAroundPlayer);
+		//this.yaw = 180 - (player.getRotY() + angleAroundPlayer);
+		this.yaw = 180 - angleAroundPlayer;
 	}
 	
 	private float calculateHorizontalDistance() {
